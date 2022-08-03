9aed4fbbb52751b557da54a949ef3168b6b16cc0
KOOHYO <zpfhzpfh112@naver.com>
2022-08-03 11:09:10 +0900

    커넥션 테스트 완료

--- src/main/java/com/iu/start/test/DBConnectionTest.java
+++ src/main/java/com/iu/start/test/DBConnectionTest.java
@@ -1,9 +1,20 @@
 package com.iu.start.test;
 
+import java.sql.Connection;
+
+import com.iu.start.util.DBConnector;
+
 public class DBConnectionTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//DBC클래스에 있는 겟을 호출!
+		try {
+			Connection con = DBConnector.getConnection();
+			System.out.println(con != null);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
