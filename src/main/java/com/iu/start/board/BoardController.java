f6fd031e725d32127530ccfd3c06c583318eecd6
KOOHYO <zpfhzpfh112@naver.com>
2022-08-15 23:20:21 +0900

    리스트 페이지

--- src/main/java/com/iu/start/board/BoardController.java
+++ src/main/java/com/iu/start/board/BoardController.java
@@ -2,11 +2,15 @@ package com.iu.start.board;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value = "/board/*")
 public class BoardController {
 
-	
+	@RequestMapping (value = "list.iu", method = RequestMethod.GET)
+	public void getList()throws Exception {
+		System.out.println("list 접속");
+	}
 	
 }
