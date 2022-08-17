512a951edba26359b03aa158dc6828b82260e55f
KOOHYO <zpfhzpfh112@naver.com>
2022-08-17 09:03:57 +0900

    ㅎㅎ

--- src/main/java/com/iu/start/bankmembers/BankMembersDAO.java
+++ src/main/java/com/iu/start/bankmembers/BankMembersDAO.java
@@ -5,6 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.iu.start.util.DBConnector;
@@ -12,58 +14,19 @@ import com.iu.start.util.DBConnector;
 @Repository
 public class BankMembersDAO implements MembersDAO {
 
-	public BankMembersDTO getLogin(BankMembersDTO bankMembersDTO)throws Exception{
-		
-		Connection con = DBConnector.getConnection();
-		
-		String sql = "SELECT USERNAME, NAME FROM BANKMEMBERS WHERE USERNAME=? AND PASSWORD=?";
-		
-		PreparedStatement st = con.prepareStatement(sql);
-		
-		st.setString(1, bankMembersDTO.getUserName());
-		st.setString(2, bankMembersDTO.getPassWord());
-		
-		ResultSet rs = st.executeQuery();
-		
-		if(rs.next()) {
-			bankMembersDTO = new BankMembersDTO();
-	        bankMembersDTO.setUserName(rs.getString("USERNAME"));
-	        bankMembersDTO.setName(rs.getString("NAME"));   
-		}else {
-			bankMembersDTO=null;
-			//return null;
-		}
-		
-		return bankMembersDTO;
+	@Autowired
+	private SqlSession sqlSession;
+	private final String NAMESPACE="com.iu.start.bankmembers.BankMembersDAO."; 
+	
+	public BankMembersDTO getLogin(BankMembersDTO bankMembersDTO)throws Exception{	
+		return sqlSession.selectOne(NAMESPACE+"getLogin",bankMembersDTO);
 	}
 	
 	//bankMembaers 회원가입
 	//int타입으로 리턴하는 이유는 insert가 돼었는지 확인하는 방법이 0 아니면 1이상에 숫자가 오기 때문이다
 	@Override
 	public int setJoin(BankMembersDTO bankMembersDTO) throws Exception {
-		//1. DB 연결
-		Connection con = DBConnector.getConnection();
-		
-		//2. SQL문 작성
-		String sql = "INSERT INTO BANKMEMBERS VALUES (?, ?, ?, ?, ?)";
-		
-		//3. 미리전송
-		PreparedStatement st = con.prepareStatement(sql);
-		
-		//4. ?값 세팅
-		st.setString(1, bankMembersDTO.getUserName());
-		st.setString(2, bankMembersDTO.getPassWord());
-		st.setString(3, bankMembersDTO.getName());
-		st.setString(4, bankMembersDTO.getEmail());
-		st.setString(5, bankMembersDTO.getPhone());
-		
-		//5. 최종전송
-		int result = st.executeUpdate();
-		
-		//6. 자원해제
-		DBConnector.disConnect(st, con);
-		
-		return result;
+		return sqlSession.insert(NAMESPACE+"setJoin", bankMembersDTO);
 	}
 	
 	//검색어를 입력해서 ID를 찾기
