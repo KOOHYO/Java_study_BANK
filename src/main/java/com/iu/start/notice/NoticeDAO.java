9d0e11d34aefa041d47ad7fe9d2bb0e5115806df
KOOHYO <zpfhzpfh112@naver.com>
2022-08-21 19:04:42 +0900

    보드 노티스 수정

--- src/main/java/com/iu/start/notice/NoticeDAO.java
+++ src/main/java/com/iu/start/notice/NoticeDAO.java
@@ -13,13 +13,13 @@ import com.iu.start.util.DBConnector;
 public class NoticeDAO {
 
 	public ArrayList<NoticeDTO> getList()throws Exception{
-		ArrayList<NoticeDTO> boardDTOs = new ArrayList<NoticeDTO>();
+		ArrayList<NoticeDTO> noticeDTOs = new ArrayList<NoticeDTO>();
 		
 		//1. DB연결
 		Connection con = DBConnector.getConnection();
 		
 		//2. SQL문 작성
-		String sql = "SELECT * FROM BOARD ORDER BY BOARDNUM DESC";
+		String sql = "SELECT * FROM NOTICE ORDER BY NOTICENUM DESC";
 		
 		//3. 미리전송
 		PreparedStatement st = con.prepareStatement(sql);
@@ -30,25 +30,25 @@ public class NoticeDAO {
 		ResultSet rs = st.executeQuery();
 		
 		while(rs.next()) {
-			NoticeDTO boardDTO = new NoticeDTO();
-			boardDTO.setNoticeNum(rs.getLong("NOTICENUM"));
-			boardDTO.setNoticeTitle(rs.getString("NOTICETITLE"));
-			boardDTO.setNoticeContents(rs.getString("NOTICETCONTENTS"));
-			boardDTO.setNoticeWriter(rs.getString("BOARDUSERNAME"));
-			boardDTO.setNoticeRedate(rs.getDate("BOARDDATE"));
-			boardDTO.setNoticeHit(rs.getLong("BOARDHIT"));
-			boardDTOs.add(boardDTO);
+			NoticeDTO noticeDTO = new NoticeDTO();
+			noticeDTO.setNoticeNum(rs.getLong("NOTICENUM"));
+			noticeDTO.setNoticeTitle(rs.getString("NOTICETITLE"));
+			noticeDTO.setNoticeContents(rs.getString("NOTICETCONTENTS"));
+			noticeDTO.setNoticeWriter(rs.getString("NOTICEWRITER"));
+			noticeDTO.setNoticeRedate(rs.getDate("NOTICEREGDATE"));
+			noticeDTO.setNoticeHit(rs.getLong("NOTICEHIT"));
+			noticeDTOs.add(noticeDTO);
 		}
 		
 		//6. 자원해제
 		DBConnector.disConnect(rs, st, con);
 		
-		return boardDTOs;
+		return noticeDTOs;
 		
 	}
 	
-	public NoticeDTO getDetail(NoticeDTO boardDTO)throws Exception{
-		NoticeDTO boardDTO2=null;
+	public NoticeDTO getDetail(NoticeDTO noticeDTO)throws Exception{
+		NoticeDTO noticeDTO2=null;
 		
 		Connection con = DBConnector.getConnection();
 		
@@ -56,23 +56,23 @@ public class NoticeDAO {
 		
 		PreparedStatement st = con.prepareStatement(sql);
 		
-		st.setLong(1, boardDTO.getBoardNum());
+		st.setLong(1, noticeDTO.getNoticeNum());
 		
 		ResultSet rs = st.executeQuery();
 		
 		if(rs.next()) {
-			boardDTO2 = new NoticeDTO();
-			boardDTO2.setBoardNum(rs.getLong("BOARDNUM"));
-			boardDTO2.setBoardTitle(rs.getString("BOARDTITLE"));
-			boardDTO2.setBoardDetail(rs.getString("BOARDDETAIL"));
-			boardDTO2.setBoardUserName(rs.getString("BOARDUSERNAME"));
-			boardDTO2.setBoardDate(rs.getDate("BOARDDATE"));
-			boardDTO2.setBoardHits(rs.getInt("BOARDHITS"));
+			noticeDTO2 = new NoticeDTO();
+			noticeDTO2.setNoticeNum(rs.getLong("NOTICENUM"));
+			noticeDTO2.setNoticeTitle(rs.getString("NOTICETITLE"));
+			noticeDTO2.setNoticeContents(rs.getString("NOTICETCONTENTS"));
+			noticeDTO2.setBoardUserName(rs.getString("BOARDUSERNAME"));
+			noticeDTO2.setBoardDate(rs.getDate("BOARDDATE"));
+			noticeDTO2.setBoardHits(rs.getInt("BOARDHITS"));
 		}
 		
 		DBConnector.disConnect(rs, st, con);
 		
-		return boardDTO2;
+		return noticeDTO2;
 	}
 	
 }
